3e17a2d3dc50256d5277ab441735a90f3839d9b7
Afmobi <afmobi@afmobidemini-2.lan>
2016-07-18 14:47:41 +0800

    Init Commit

---
+++ DRJTableViewDemo/DRJTableViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DRJTableViewDemo
+//
+//  Created by Afmobi on 16/7/18.
+//  Copyright © 2016年 Afmobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
