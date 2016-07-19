56d54c62f4e1a20dc5a2be7e47aca00e1da2d13f
Afmobi <afmobi@afmobidemini-2.lan>
2016-07-19 10:09:15 +0800

    Init DRJViewControllerDemo

---
+++ DRJViewControllerDemo/DRJViewControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DRJViewControllerDemo
+//
+//  Created by Afmobi on 16/7/19.
+//  Copyright © 2016年 Afmobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
