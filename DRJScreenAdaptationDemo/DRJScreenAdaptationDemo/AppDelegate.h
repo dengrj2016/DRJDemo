1dfcefa69b47ef6118988b847f6de71be8d9899a
Afmobi <afmobi@afmobidemini-2.lan>
2016-07-19 10:11:11 +0800

    Init DRJScreenAdaptationDemo

---
+++ DRJScreenAdaptationDemo/DRJScreenAdaptationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DRJScreenAdaptationDemo
+//
+//  Created by Afmobi on 16/7/19.
+//  Copyright © 2016年 Afmobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
