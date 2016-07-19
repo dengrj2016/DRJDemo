e9245780492a6b851c0f194e749562d7b4acb7a3
frank-du <me@frank-du.com>
2016-07-19 10:50:43 +0800

    创建大纲

--- DRJScreenAdaptationDemo/DRJScreenAdaptationDemo/AutolayoutMultiplierVC.h
+++ DRJScreenAdaptationDemo/DRJScreenAdaptationDemo/AutolayoutMultiplierVC.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  AutolayoutMultiplierVC.h
 //  DRJScreenAdaptationDemo
 //
-//  Created by Afmobi on 16/7/19.
+//  Created by Frank Du on 16/7/19.
 //  Copyright © 2016年 Afmobi. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface AutolayoutMultiplierVC : UIViewController
 
 @end
-
---
+++ DRJScreenAdaptationDemo/DRJScreenAdaptationDemo/CodeVC.h
@@ -0,0 +1,13 @@
+//
+//  CodeVC.h
+//  DRJScreenAdaptationDemo
+//
+//  Created by Frank Du on 16/7/19.
+//  Copyright © 2016年 Afmobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CodeVC : UIViewController
+
+@end
