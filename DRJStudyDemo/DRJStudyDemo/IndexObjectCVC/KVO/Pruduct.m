49283f9ca40a58c1389a85d077f90d3633b30324
Afmobi <afmobi@afmobidemac-mini-2.local>
2016-07-28 17:31:40 +0800

    单例优化

--- DRJStudyDemo/DRJStudyDemo/IndexObjectCVC/KVO/Pruduct.m
+++ DRJStudyDemo/DRJStudyDemo/IndexObjectCVC/KVO/Pruduct.m
@@ -8,13 +8,27 @@
 
 #import "Pruduct.h"
 
-static id instance=nil;
+//static id instance=nil;
 @implementation Pruduct
 
-+(id)getInstance{
-    if (!instance) {
-        instance=[[super alloc] init];
-    }
+//+(id)getInstance{
+//    if (!instance) {
+//        instance=[[super alloc] init];
+//    }
+//    return instance;
+//}
+
++ (id)getInstance{
+    static id instance = nil;
+    static dispatch_once_t once;
+    dispatch_once(&once, ^{
+        instance = [[self.class alloc]init];
+    });
     return instance;
 }
+
+- (void)dealloc {
+    // 永远不要调用这个方法
+}
+
 @end
